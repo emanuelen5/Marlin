10b7dcfe8a9d7ba7021836316cd2eebf1e8fe4ae
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2022-12-26 17:45:55 +0100

    Add random generated UUID

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -140,7 +140,7 @@
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
-//#define MACHINE_UUID "00000000-0000-0000-0000-000000000000"
+#define MACHINE_UUID "f213fa7e-9d43-411c-b89b-a24343fd6a9f"
 
 // @section stepper drivers
 
